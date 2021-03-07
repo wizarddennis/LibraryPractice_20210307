1659317a275246a9b2a1a13abe5d640b52234f98
dennis <travlInfo4you@gmail.com>
2021-03-07 15:50:12 +0900

    전화번호 기능 만들기

--- app/src/main/java/kr/co/tjoeun/librarypractice_20210307/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/librarypractice_20210307/MainActivity.kt
@@ -1,6 +1,7 @@
 package kr.co.tjoeun.librarypractice_20210307
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,6 +16,13 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+        // 전화걸기 버튼 눌리면 => 실제 전화 걸어보자.
+        callBtn.setOnClickListener {
+            val myUri = Uri.parse("tel:010-2222-3333")
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
+        }
+
         // 이미지뷰를 눌렀을때? => 사진 보기 화면 이동.
         profileImg.setOnClickListener {
             val myIntent = Intent(mContext, ViewPhotoActivity::class.java)
