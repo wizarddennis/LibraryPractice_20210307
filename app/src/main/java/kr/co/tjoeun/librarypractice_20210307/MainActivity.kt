25083071169613388a3f61497caaf9ab1636b550
dennis <travlInfo4you@gmail.com>
2021-03-07 16:40:55 +0900

    최근 활동 사진 표시 공간(이미지뷰) 추가

--- app/src/main/java/kr/co/tjoeun/librarypractice_20210307/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/librarypractice_20210307/MainActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +22,28 @@ class MainActivity : BaseActivity() {
     override fun setupEvents() {
         // 전화걸기 버튼 눌리면 => 권한이 있는지 확인하고 => 실제 전화 걸어보자.
         callBtn.setOnClickListener {
-            val myUri = Uri.parse("tel:010-2222-3333")
-            val myIntent = Intent(Intent.ACTION_CALL, myUri)
-            startActivity(myIntent)
+//            1. 권한이 있는지/거절됬는지에 따른 [행동 방침](Interface) 변수 생성
+//            권한이 있으면 => Uri / ACTION_CALL 등등 내용 실행
+//            권한이 없으면 => 토스트로 연결 불가 안내.
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+                    val myUri = Uri.parse("tel: 010-2222-3333")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "권한이 없어서 전화 연결불가.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
+//            2. 1에서 만든 [행동 방침]을 가지고 => 실제 권한 확인.
+            TedPermission.with(mContext)
+                .setPermissionListener(pl)
+                .setDeniedMessage("권한을 거부하면 연결불가합니다. [설정] > [권한]에서 셋팅해주세요.")
+                .setPermissions(android.Manifest.permission.CALL_PHONE)
+                .check()
         }
 
         // 이미지뷰를 눌렀을때? => 사진 보기 화면 이동.
